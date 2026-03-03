ca14c2d32ebfd6a58d9da4d301714063839d7c1b
Isaque de Sousa <isaque.error404@gmail.com>
2026-03-03 16:55:40 -0300

    feat: implement CreateWorkoutPlan use case with transaction handling

---
+++ src/usecases/CreateWorkoutPlan.ts
@@ -0,0 +1,67 @@
+import { WeekDay } from "../generated/prisma/enums.js";
+import { prisma } from "../lib/db.js";
+
+interface CreateWorkoutPlanDto {
+  userId: string;
+  name: string;
+  workoutDays: Array<{
+    name: string;
+    weekDay: WeekDay;
+    isRest: boolean;
+    estimatedDurationInSeconds: number;
+    exercises: Array<{
+      order: number;
+      name: string;
+      sets: number;
+      reps: number;
+      restTimeInSeconds: number;
+    }>;
+  }>;
+}
+
+export class CreateWorkoutPlan {
+  async execute(dto: CreateWorkoutPlanDto) {
+    const existingWorkoutPlan = await prisma.workoutPlan.findFirst({
+      where: {
+        isActive: true,
+      },
+    });
+
+    return prisma.$transaction(async (tx) => {
+      if (existingWorkoutPlan) {
+        await tx.workoutPlan.update({
+          where: {
+            id: existingWorkoutPlan.id,
+          },
+          data: { isActive: false },
+        });
+      }
+
+      const result = await tx.workoutPlan.create({
+        data: {
+          name: dto.name,
+          userId: dto.userId,
+          isActive: true,
+          workoutDays: {
+            create: dto.workoutDays.map((workoutDay) => ({
+              name: workoutDay.name,
+              weekDay: workoutDay.weekDay,
+              isRest: workoutDay.isRest,
+              estimatedDurationInSeconds: workoutDay.estimatedDurationInSeconds,
+              exercises: {
+                create: workoutDay.exercises.map((exercise) => ({
+                  order: exercise.order,
+                  name: exercise.name,
+                  sets: exercise.sets,
+                  reps: exercise.reps,
+                  restTimeInSeconds: exercise.restTimeInSeconds,
+                })),
+              },
+            })),
+          },
+        },
+      });
+      return result;
+    });
+  }
+}
